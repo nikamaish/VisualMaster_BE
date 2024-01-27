9a724326b16b641cdb61e65f9bfd57520f247efb
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-27 12:52:54 +0530

    Add Route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import CryptoGraphs from './Cryptographs'
-import Services from './services/Services'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CryptoGraphs from "./Cryptographs";
+import Services from "./services/Services";
 // import cryptoData from './cryptoData/Cryptodata'
-import './broadcast/Broadcast' 
-import Broadcast from './broadcast/Broadcast'
-import Headsection from './headSection/Headsection'
-import Navbar from './navbar/Navbar'
+import "./broadcast/Broadcast";
+import Broadcast from "./broadcast/Broadcast";
+import Headsection from "./headSection/Headsection";
+import Navbar from "./navbar/Navbar";
 // import RealTimeData from './cryptoData/Cryptodata'
 // import CryptoWidget from './cryptoData/CryptoWidget'
-import Footer from './Footer/Footer'
-import RiskWarning from './Risk/Risk'
-import Help from './help/Help'
-import CryptoData from './cryptoData/CryptoData'
-import CryptoRest from './CryptoRest'
-
-
+import Footer from "./Footer/Footer";
+import RiskWarning from "./Risk/Risk";
+import Help from "./help/Help";
+import CryptoData from "./cryptoData/CryptoData";
+import CryptoRest from "./CryptoRest";
+import Signin from "./Signin/Signin";
+import Signup from "./Signup/Signup";
 
 const App = () => {
   return (
-    <div className='App'>     
-  {/* <Services/>
+    <div className="App">
+      {/* <Services/>
   <CryptoWidget/>
   <RealTimeData/> */}
-  
-  
-  <Navbar/>
-  {/* <CryptoGraphs/>  */}
-  {/* <Broadcast/> */}
-  {/* <Headsection/> */}
-  {/* <CryptoData/> */}
-  {/* <Services/> */}
-  {/* <Help/> */}
-  {/* <RiskWarning/> */}
-  {/* <Footer/> */}
-  <CryptoRest/>
 
-
-
-  
+      <Navbar />
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {/* <CryptoGraphs/>  */}
+      {/* <Broadcast/> */}
+      {/* <Headsection/> */}
+      {/* <CryptoData/> */}
+      {/* <Services/> */}
+      {/* <Help/> */}
+      {/* <RiskWarning/> */}
+      {/* <Footer/> */}
+      <CryptoRest />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
